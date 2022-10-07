ffff8a12c78477e13be5ff4c662e64f2e7ff1340
RamachandranRachana <rachanaramachandran24@gmail.com>
2022-10-07 12:05:59 -0400

    Regex Class Usage
    Using this class I parse input to decipher an encrypted  motivating quote!

---
+++ RegexExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\racha\Documents\GitHub\CSharp-Journey\input.txt");
+            foreach (string l in lines)
+            {
+                if (l.Contains("split"))
+                {
+                    string[] outs = l.Split(" ");
+                    Console.WriteLine(outs[4]);
+                }
+            }
+
+            string pattern = @"\d{2,3}";
+            Regex regex = new Regex(pattern);
+
+            string[] lines2 = System.IO.File.ReadAllLines(@"C:\Users\racha\Documents\GitHub\CSharp-Journey\input2.txt");
+            foreach (string l in lines2)
+            {
+                MatchCollection matches = regex.Matches(l);
+                foreach (Match match in matches)
+                {
+                    int iVal = int.Parse(match.Value);
+                    Console.Write((char)iVal);
+                }
+            }
+        }
+    }
+}
