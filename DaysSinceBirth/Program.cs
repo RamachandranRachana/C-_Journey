ec3ab1487fb1cde1fa4a47782c60996758c36d4a
RamachandranRachana <rachanaramachandran24@gmail.com>
2022-10-07 12:52:58 -0400

    DaysSinceBirth
    This program computes the day since you are born.

---
+++ DaysSinceBirth/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DaysSinceBirth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birthday in format yyyy-mm-dd");
+            string input = Console.ReadLine();
+            DateTime dateTime;
+            if (DateTime.TryParse(input, out dateTime))
+            {
+                Console.WriteLine("Number of Days since Birth: {0}",DateTime.Now.Subtract(dateTime).Days);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!!");
+            }
+        }
+    }
+}
