64edf7c340aad51c1aa49479c71ad32e652623db
RamachandranRachana <rachanaramachandran24@gmail.com>
2022-10-11 14:48:51 -0400

    Delegate Example 1
    Using the Lists RemoveAll by passing Filter method as parameter

---
+++ DelegateEx1RemoveNameFromList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegateEx1RemoveNameFromList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "Rachana", "Rajdeep", "Diploo", "Keerthana", "Arpana" };
+
+            Console.Write("before filter: ");
+
+            //before filter
+            foreach (string s in list)
+            {
+                Console.Write(s + " ");
+            }
+
+            Console.WriteLine();
+
+            //apply filter
+            list.RemoveAll(Filter);
+
+            Console.Write("after filter: ");
+            //after filter
+            foreach (string s in list)
+            {
+                Console.Write(s + " ");
+            }
+
+            Console.WriteLine();
+
+        }
+
+        static bool Filter(string s)
+        {
+            return s.Contains("r");
+        }
+    }
+}
